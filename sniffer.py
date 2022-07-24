d478648e4424c45f1e85f805cb64f318aa16273e
jayKlo <jay@jayklo.com>
2022-07-24 01:20:29 -0400

    Sniffer additions

---
+++ sniffer.py
@@ -0,0 +1,30 @@
+import socket
+import os
+
+# Host to listen on
+HOST = '192.168.110.128'
+
+def main():
+    # Create raw socket, bin to public interface
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((HOST, 0))
+    # Include the IP header in the capture
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    
+    # Read one packet
+    print(sniffer.recvfrom(65565))
+
+    # If on Windows, turn off promiscious mode
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+if __name__ == '__main__':
+    main()
